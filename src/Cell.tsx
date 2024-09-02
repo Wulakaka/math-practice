0cef17ed7740e2222b5b4716f381106ee8c1c25d
张伟峰 <wulakaka@live.com>
2024-09-02 20:50:32 +0800

    feat: 调整颜色

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -2,8 +2,9 @@ export default function Cell({children}: { children: string | number | undefined
     const isNumber = typeof children === 'number'
     const text = (children ?? '').toString().padStart(2).split('')
 
-    // const color = `hsl(${Math.random() * 360}deg,100%,50%)`
-    const color = 'darkgreen'
+    // 色相、饱和度、亮度
+    const color = `hsl(${Math.random() * 360}deg,80%,50%)`
+    // const color = 'darkgreen'
     return (
         <svg width="100" height="100" viewBox="-0.5,-0.5,100,100" className="-mr-[1px] last:mr-0">
             <rect x1="-0.5" y1="-0.5" width="99" height="99" fill="none" stroke="darkgreen"></rect>
@@ -20,5 +21,4 @@ export default function Cell({children}: { children: string | number | undefined
             </g>
         </svg>
     )
-
 }
